9c5ba3d43c822c717c5fdfcb7299abe4563bb604
Michael Longstreth <mlongstreth@Michaels-MacBook-Pro.local>
2021-12-27 19:51:55 -0800

    auth working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytutorial393c1d49": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
